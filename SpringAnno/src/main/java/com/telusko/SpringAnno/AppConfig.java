f91ddd4f092845fc7de6b313c518be744a547e69
Ajit404 <54814535+Ajit404@users.noreply.github.com>
2019-09-15 17:49:46 +0530

    Add files via upload

---
+++ SpringAnno/src/main/java/com/telusko/SpringAnno/AppConfig.java
@@ -0,0 +1,10 @@
+package com.telusko.SpringAnno;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.telusko.SpringAnno")
+public class AppConfig {
+
+}
